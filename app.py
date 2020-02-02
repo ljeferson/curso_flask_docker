f1360894b1fc5791ccbf1aeadbdc672f264c87de
Lucas <lucasjeferson6000@gmail.com>
2020-02-01 21:49:46 -0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def showname():
+    name = request.args.get("name")
+    return f"Name: {name}"
+
+@app.route("/name/<name>")
+def showname_param(name):
+    return name
+
+app.run(debug=True, port=3000, host="0.0.0.0")
