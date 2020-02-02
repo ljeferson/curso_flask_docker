9cade0622d1fd1df9f9ce27f95e77639da9823a9
Lucas <lucasjeferson6000@gmail.com>
2020-02-01 22:20:51 -0300

    Aula 11

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 
 app = Flask(__name__)
+
+
+
+@app.route("/home")
+def home():
+    return Response("Home Page", 200, {})
+
 
 @app.route("/")
 def showname():
     name = request.args.get("name")
     return f"Name: {name}"
 
+@app.route("/name")
 @app.route("/name/<name>")
-def showname_param(name):
-    return name
+def showname_param(name = None):
+    if name:
+        return name
+    return "Nome não foi preenchido!"
 
 app.run(debug=True, port=3000, host="0.0.0.0")
